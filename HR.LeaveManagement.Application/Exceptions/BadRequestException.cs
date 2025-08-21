06f18302c3d9b8cc0418543182939f309a7a5928
aungkhant2002 <aung.khant@ucsy.edu.mm>
2025-08-21 22:14:32 +0630

    feat(validation): add validation for LeaveType creation using FluentValidation

---
+++ HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+        ValidationErrors = new List<string>();
+    }
+
+    public BadRequestException(string message, ValidationResult validationResult) : base(message)
+    {
+        ValidationErrors = new();
+        foreach (var error in validationResult.Errors)
+        {
+            ValidationErrors.Add(error.ErrorMessage);
+        }
+    }
+
+    public List<string> ValidationErrors { get; set; }
+}
